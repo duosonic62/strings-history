a95a9ad510bb7650fa558a2cbec546ce45797c60
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-09-22 20:30:53 +0900

    first commit

---
+++ src/main/kotlin/com/littlefeet/stringshistory/StringsHistoryApplication.kt
@@ -0,0 +1,11 @@
+package com.littlefeet.stringshistory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StringsHistoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<StringsHistoryApplication>(*args)
+}
---
+++ src/test/kotlin/com/littlefeet/stringshistory/StringsHistoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.littlefeet.stringshistory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StringsHistoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
