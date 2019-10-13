d18fdc78e5872d550b737117928d8477bcabeeb1
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-10-14 08:21:17 +0900

    add service

--- src/main/kotlin/com/littlefeet/domain/service/MemberService.kt
+++ src/main/kotlin/com/littlefeet/domain/service/MemberService.kt
@@ -1,9 +1,12 @@
 package com.littlefeet.domain.service
 
 import com.littlefeet.api.models.MemberInformation
+import com.littlefeet.api.models.MemberPostParameter
 import com.littlefeet.domain.CommonHeader
 import com.littlefeet.domain.converter.MemberConverter
+import com.littlefeet.domain.entity.Member
 import com.littlefeet.domain.repository.MemberDao
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,4 +17,17 @@ class MemberService(
     MemberConverter.convertShowMember(
       memberDao.selectByToken(commonHeader.token)
     )
+
+  fun create(
+    commonHeader: CommonHeader,
+    memberPostParameter: MemberPostParameter
+  ): HttpStatus {
+    val insert = memberDao.insert(
+      Member().apply {
+        name = memberPostParameter.name
+      }
+    )
+    return if (insert == 1) HttpStatus.OK
+    else HttpStatus.INTERNAL_SERVER_ERROR
+  }
 }
