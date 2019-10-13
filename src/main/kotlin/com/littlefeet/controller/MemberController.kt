3da7d3a8f56bc7e5cf0c789c96bd0cbd3142705a
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-10-14 08:21:17 +0900

    add service

--- src/main/kotlin/com/littlefeet/controller/MemberController.kt
+++ src/main/kotlin/com/littlefeet/controller/MemberController.kt
@@ -4,6 +4,7 @@ import com.littlefeet.api.models.MemberPostParameter
 import com.littlefeet.domain.CommonHeader
 import com.littlefeet.domain.service.MemberService
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -26,6 +27,6 @@ class MemberController(
   fun createMembere(
     @RequestHeader httpHeaders: HttpHeaders,
     @Valid @RequestBody memberPostParameter: MemberPostParameter
-  ) = memberService.create(CommonHeader.of(httpHeaders), memberPostParameter)
+  ): HttpStatus = memberService.create(CommonHeader.of(httpHeaders), memberPostParameter)
 }
 
