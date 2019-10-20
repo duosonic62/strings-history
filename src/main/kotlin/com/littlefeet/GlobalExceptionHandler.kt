c93ab28dcb21e1376a04df4e3827831dbd1875d9
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-10-20 10:55:23 +0900

    add exception

--- src/main/kotlin/com/littlefeet/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/littlefeet/GlobalExceptionHandler.kt
@@ -1,5 +1,7 @@
 package com.littlefeet
 
+import com.littlefeet.api.models.Error
+import com.littlefeet.domain.exception.DbException
 import com.littlefeet.util.KtLog
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -9,12 +11,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class GlobalExceptionHandler {
   companion object : KtLog()
 
+  @ExceptionHandler(DbException::class)
+  fun handleDbException(
+    ex: DbException
+  ): Error {
+    log.error(ex)
+    return com.littlefeet.api.models.Error(
+      code = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+      message = ex.message
+    )
+  }
+
   @ExceptionHandler(Exception::class)
   fun handleException(
     ex: java.lang.Exception
-  ): com.littlefeet.api.models.Error {
+  ): Error {
     log.error(ex)
-    return com.littlefeet.api.models.Error(
+    return Error(
       code = HttpStatus.INTERNAL_SERVER_ERROR.value(),
       message = "Internal Server Error."
     )
