9cc1201ebaebca160105851016e3efee5620d441
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-10-27 10:37:27 +0900

    fix converter

--- src/main/kotlin/com/littlefeet/domain/converter/MemberConverter.kt
+++ src/main/kotlin/com/littlefeet/domain/converter/MemberConverter.kt
@@ -5,28 +5,57 @@ import com.littlefeet.api.models.MemberPostParameter
 import com.littlefeet.api.models.MemberPutParameter
 import com.littlefeet.domain.entity.Member
 
+/**
+ * 会員情報のオブジェクト入れ替えを行うクラス
+ */
 object MemberConverter {
-  fun convertShowMember(
+  /**
+   * DBのエンティティをアプリで表示する会員情報にコンバートする
+   *
+   * @param member
+   * @return アプリ表示会員情報
+   */
+  fun convertShowMemberResponse(
     member: Member
   ): MemberInformation =
     MemberInformation(
       name = member.name
     )
 
-  fun convertCreateMember(
+  /**
+   * APIからの作成リクエストをDBのエンティティに変更
+   *
+   * @param memberPostParameter
+   * @return 会員情報DBエンティティ
+   */
+  fun convertCreateMemberParameter(
     memberPostParameter: MemberPostParameter
   ): Member = Member().apply {
     name = memberPostParameter.name
   }
 
-  fun convertUpdateMember(
+  /**
+   * APIからの変更リクエストをDBのエンティティに変更
+   *
+   * @param memberPutParameter
+   * @param member
+   * @return 会員情報DBエンティティ
+   */
+  fun convertUpdateMemberParameter(
     memberPutParameter: MemberPutParameter,
     member: Member
   ): Member = member.apply {
     name = memberPutParameter.name
   }
 
-  fun convertDeleteMember(
+  /**
+   * APIからの削除リクエストをDBのエンティティに変更
+   *
+   * @param memberPutParameter
+   * @param member
+   * @return 会員情報DBエンティティ
+   */
+  fun convertDeleteMemberParameter(
     member: Member
   ): Member = member.apply { isDeleted = true }
 }
