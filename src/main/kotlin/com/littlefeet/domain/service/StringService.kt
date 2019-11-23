cc1fdf9ebeefe4f1152ba3d8ec161e8cfd806ef2
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-11-23 10:51:56 +0900

    add create string param converter

--- src/main/kotlin/com/littlefeet/domain/service/StringService.kt
+++ src/main/kotlin/com/littlefeet/domain/service/StringService.kt
@@ -3,7 +3,7 @@ package com.littlefeet.domain.service
 import com.littlefeet.api.models.StringInformation
 import com.littlefeet.api.models.StringRegisterParameter
 import com.littlefeet.domain.CommonHeader
-import com.littlefeet.domain.entity.GuitarString
+import com.littlefeet.domain.converter.StringConverter
 import com.littlefeet.domain.repository.GuitarStringDao
 import org.springframework.stereotype.Service
 
@@ -11,18 +11,18 @@ import org.springframework.stereotype.Service
 class StringService(
   private val stringDao: GuitarStringDao
 ) {
+  /**
+   * 弦情報を作成
+   *
+   * @param commonHeader
+   * @param stringRegisterParameter
+   * @return 作成した弦情報
+   */
   fun create(
     commonHeader: CommonHeader,
     stringRegisterParameter: StringRegisterParameter
   ): StringInformation {
-    val param = GuitarString().apply {
-      name = stringRegisterParameter.name
-      description = stringRegisterParameter.description
-      maker = stringRegisterParameter.maker
-      thickGauge = stringRegisterParameter.thinGauge.toShort()
-      thickGauge = stringRegisterParameter.thickGauge.toShort()
-      url = stringRegisterParameter.url
-    }
+    val param = StringConverter.convertCreateStringParameter(stringRegisterParameter)
     stringDao.insert(param)
     return StringInformation(
       id = param.id,
