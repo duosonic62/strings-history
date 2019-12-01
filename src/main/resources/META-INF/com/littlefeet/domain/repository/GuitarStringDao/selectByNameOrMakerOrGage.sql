7f219ab2f613d14f5f90b8f310896a8023fba5d4
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-12-01 11:29:48 +0900

    add insert data

--- src/main/resources/META-INF/com/littlefeet/domain/repository/GuitarStringDao/selectByNameOrMakerOrGage.sql
+++ src/main/resources/META-INF/com/littlefeet/domain/repository/GuitarStringDao/selectByNameOrMakerOrGage.sql
@@ -3,15 +3,15 @@ select
 from guitar_string
 where is_deleted = false
   /*%if name != null */
-  and name like /* @contain(name) */'%X%' escape '$'
+  and name like /* @infix(name) */'Eatrhwood'
   /*%end*/
   /*%if maker != null */
-  and maker like /* @contain(maker) */'%X%' escape '$'
+  and maker like /* @infix(maker) */'ERNIEBALL'
     /*%end*/
     /*%if thinGauge != null */
-    and thin_gauge =/*thinGauge*/10
+  and thin_gauge =/*thinGauge*/12
     /*%end*/
     /*%if thinGauge != null */
-    and thick_gauge =/*thinGauge*/46
+  and thick_gauge =/*thickGauge*/54
 /*%end*/
 
