16d24d71285fc165a3360a0e2ddc8f3e3192f293
Sekiguchi Riku <l.life.style.of.sustainability@gmail.com>
2019-12-21 11:22:31 +0900

    fix typo

--- src/main/kotlin/com/littlefeet/infrastracture/DefaultGuitartRepository.kt
+++ src/main/kotlin/com/littlefeet/infrastracture/DefaultGuitartRepository.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component
 /**
  * GuitarRepositoroyのコンクリートクラス
  *
- * @property guitarRepository
+ * @property guitarDao
  */
 @Component
 class DefaultGuitartRepository(
